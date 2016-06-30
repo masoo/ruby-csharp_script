a447f7874260fbfe1dbf60e61663a6e14ad32554
FUNABARA Masao <masao@masoo.jp>
2016-06-30 23:09:44 +0900

    setting COM.

--- ruby-csharp_script/Properties/AssemblyInfo.cs
+++ ruby-csharp_script/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
 // 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
 // その型の ComVisible 属性を true に設定してください。
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
 [assembly: Guid("26ef5196-b3b6-4cd3-ba09-c8d9d4ed4cc0")]
