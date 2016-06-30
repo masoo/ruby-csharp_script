f265271a1e23e48fd3a18a0ac64a78b24ef93d66
FUNABARA Masao <masao@masoo.jp>
2016-06-30 21:42:08 +0900

    add CsharpScript wrapper.

--- ruby-csharp_script/csharp_script/CsharpScript.cs
+++ ruby-csharp_script/csharp_script/CsharpScript.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Scripting = Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace ruby.csharp_script
 {
-    class CsharpScript
+    /// <summary>
+    /// CSharpScript wrapper.
+    /// </summary>
+    public static class CSharpScript
     {
+        /// <summary>
+        /// Create a new C# script.
+        /// </summary>
+        /// <param name="code">The source code of the script.</param>
+        /// <returns></returns>
+        public static object Create(string code)
+        {
+            return Scripting.CSharpScript.Create(code);
+        }
+
+        /// <summary>
+        /// Run a C# script.
+        /// </summary>
+        /// <param name="code">The source code of the script.</param>
+        /// <returns></returns>
+        public static object RunAsync(string code)
+        {
+            return Scripting.CSharpScript.RunAsync(code);
+        }
+
+        /// <summary>
+        /// Run a C# script and return its resulting value.
+        /// </summary>
+        /// <param name="code">The source code of the script.</param>
+        /// <returns>Returns the value returned by running the script.</returns>
+        public static object EvaluateAsync(string code)
+        {
+            return Scripting.CSharpScript.EvaluateAsync(code);
+        }
     }
 }
