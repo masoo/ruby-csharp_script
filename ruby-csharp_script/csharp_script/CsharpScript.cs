2d360bbb92ada239a4609c5f020438b1777a0cfa
FUNABARA Masao <masao@masoo.jp>
2016-06-30 18:30:08 +0900

    add CsharpScript.cs

--- ruby-csharp_script/csharp_script/CsharpScript.cs
+++ ruby-csharp_script/csharp_script/CsharpScript.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ruby_csharp_script
+namespace ruby.csharp_script
 {
-    public class Class1
+    class CsharpScript
     {
     }
 }
