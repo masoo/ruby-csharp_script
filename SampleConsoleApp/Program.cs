1308cfab1cbbbe96d598a8aa3b16934844199413
FUNABARA Masao <masao@masoo.jp>
2017-06-20 17:27:28 +0900

    add sample code.

---
+++ SampleConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using ruby.csharp_script;
+
+namespace SampleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csharp_script = new CSharpScript();
+            var result = csharp_script.EvaluateAsync("1+1");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
