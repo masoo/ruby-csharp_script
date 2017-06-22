9e718e7dcc00e77e3a0855bcec77b58e0b35277d
FUNABARA Masao <masao@masoo.jp>
2017-06-22 16:27:08 +0900

    add rubygem.

--- rubygem/ruby-csharp_script/lib/ruby/csharp_script.rb
+++ rubygem/ruby-csharp_script/lib/ruby/csharp_script.rb
@@ -1,7 +1,17 @@
 require "ruby/csharp_script/version"
+require 'win32ole'
 
 module Ruby
-  module CsharpScript
-    # Your code goes here...
+  module CSharpScript
+    class CSharpScript
+      attr_reader :csharp_script
+      def initialize
+        @csharp_script = WIN32OLE.new("ruby.csharp_script.CSharpScript")
+      end
+      
+      def evaluate_async(code)
+        @csharp_script.EvaluateAsync(code)
+      end
+    end
   end
 end
